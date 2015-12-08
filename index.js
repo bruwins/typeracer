d09ad700e3dc51fa3fd8b00355459b36e85c4d18
Richard Ho <richard.ho@moblab.com>
2015-12-08 01:02:14 -0800

    Why isn't setex working with redis cloud???

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ router.route('/')
         // Uses a setex so that it disappears
         var text = "";
         client.exists(promptKey+keySignature, function(err, playing) {
-            console.log("PLAYING: ", playing);
+            console.log("PLAYING: ", playing, promptKey+keySignature);
             if(playing) {
                 text = matchAnswer(username, keySignature, command, res);
             } else if(command.indexOf("start race") === 0) {
